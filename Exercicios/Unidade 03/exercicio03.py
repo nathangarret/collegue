89bdc9164f1e6913da519cb721c6d5a6ddab64dd
Nathan Vasconcelos Garrett <ngarrett768@gmail.com>
2024-10-28 21:47:18 -0300

    Unidade 03 - Exercícios

---
+++ Exercicios/Unidade 03/exercicio03.py
@@ -0,0 +1,9 @@
+""" 
+Especificação
+Escreva uma função chamada countWord() que recebe dois parâmetros: o primeiro é o nome de um arquivo (e.g., texto.txt), e o segundo é uma palavra a ser procurada no arquivo. A sua função deve retornar o número de vezes que a palavra dada aparece no arquivo, como a seguir:
+
+>>> countWord('gettysburg.txt', 'people')
+3
+Orientações
+Vamos assumir que esse arquivo está na mesma pasta do seu programa para este problema. Note que existem 3 arquivos para teste incluídos neste exercício (blowinginthewind.txt, gettysburg.txt e moinho.txt).
+"""
